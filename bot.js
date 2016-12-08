573177e5eb759b181b47c893d15778366839d827
cgadgetz <cgadgetz@gmail.com>
2016-12-07 20:14:51 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ function respond() {
       botRegexSiege = /^\/siege/; botRegexOW = /^\/ratings/; 
       botRegexSlut = /^\/slut/; botRegex4thdown = /^\/4th down/;
       botRegexProp = /^\/prop/;botRegexKys = /^\/kys/; botRegexDnuts = /^\/who/; botRegexBrye = /^\/brye/;
-      botRegexGian = /^\/gian/; botRegexScrotes = /^\/scrotes/; botRegexShaki = /^\/shaki/;
+      botRegexGian = /^\/gian/; botRegexFU621 = /^\/FU/; botRegexShaki = /^\/shaki/;
       botRegexDaf = /^\/dafuq/; botRegexAyy = /^\/ayy/; botRegexSchedule = /^\/schedule/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -65,7 +65,7 @@ function respond() {
   } 
   else if(request.text && botRegexScrotes.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://i.groupme.com/1006x1340.jpeg.730381e4280e4028b8375432af14f39f.large");
+    postMessage("https://www.youtube.com/watch?v=aJY4AOFoaZI");
     this.res.end();
   } 
   else if(request.text && botRegexShaki.test(request.text)) {
